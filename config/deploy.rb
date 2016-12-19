21e3ef1753ac1a4ec61760b4cd37b55878b8a0a3
khoatran <thanhkhoa.tran@citynow.vn>
2016-12-19 13:16:32 +0700

    Config deploy 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server 'ec2-54-249-68-119.ap-northeast-1.compute.amazonaws.com', port: 80, roles: [:web, :app, :db], primary: true
+server '172.31.19.116', port: 443, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@gitlab.com:khoatt/exchange.git'
 set :application,     'exchange'
